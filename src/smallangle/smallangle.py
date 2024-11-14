a913799f35a06de3786cd3bb8f8818d66795d51d
Ralfphf <ralfy.arny@gmail.com>
2024-11-14 12:01:56 +0100

    Addition of comments
    For further understanding of the code, comments were added

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,12 +3,16 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+#Creates a click group for the main commando
 @click.group()
 def calculation_group():
     pass
 
+#Sub-commando for the function sin with NUMBER as options
 @calculation_group.command()
-@click.option("-n", "--number", default = 10)   
+@click.option("-n", "--number", default = 10) 
+
+#calculates the sin(x) with x between 0 and 2pi in steps given by the option NUMBER
 def sin(number):
     """calculate sin(x) for 0 < x < 2pi in NUMBER steps
 
@@ -18,8 +22,12 @@ def sin(number):
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
+
+#Sub-commando for the function tan with NUMBER as options
 @calculation_group.command()
 @click.option("-n", "--number", default = 10)
+
+#calculates the sin(x) with x between 0 and 2pi in steps given by the option NUMBER
 def tan(number):
     """calculate tan(x) for 0 < x < 2pi in NUMBER steps
 
@@ -29,6 +37,6 @@ def tan(number):
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
-
+#If the function 'calculation_group' gets used in another file, this script will not be run
 if __name__ == "__main__":
     calculation_group()
