3102362c929740b351e5abc41ebda57d872497fd
Ralfphf <ralfy.arny@gmail.com>
2024-11-14 11:26:17 +0100

    Addition of help text at every sub-commando
    Giving the description of the variable number in context

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -8,14 +8,20 @@ def calculation_group():
     pass
 
 @calculation_group.command()
-@click.option("-n", "--number", default = 10)
+@click.option("-n", "--number", default = 10,
+    help="Number of steps between 0 and 2pi.",
+    show_default=True,
+    )   
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
 @calculation_group.command()
-@click.option("-n", "--number", default = 10)
+@click.option("-n", "--number", default = 10,
+    help="Number of steps between 0 and 2pi.",
+    show_default=True,
+    )
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
